d826b6f635335156564f95bf27310baa0f0ab6c0
Prince637-boo <i8674333@gmail.com>
2025-08-07 22:40:25 +0000

    ajout d'un scriot de traitement d'image

--- trésor2.py
+++ trésor2.py
@@ -1,1 +1,9 @@
-print("hello word")
+from skimage import data
+from skimage import color
+import matplotlib.pyplot as plt
+
+image = data.astronaut()  # image d'exemple sous forme de tableau NumPy
+gray_image= color.rgb2gray(image)
+plt.imshow(gray_image, cmap='gray')
+plt.axis('off')
+plt.show()
